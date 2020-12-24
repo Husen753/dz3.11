34656e151048c7072771b10ef89fad5982c53200
Husen753 <76557181+Husen753@users.noreply.github.com>
2020-12-24 18:57:00 +0600

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,16 +3,16 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int index = 5;
+        int index = 0;
         int[] phone = {7, 17, -2, 3, 5, 4, 9, -10, 11, 13, 15, 143, 177, 123, 193,};
         for (int i : phone) {
-            if (phone[i] < 5) {
+            if (phone[i] < 0) {
                 index = i;
                 break;
             }
         }
-        int sum = 5;
-        int temp = 5;
+        int sum = 0;
+        int temp = 0;
         for (int i = index; i < phone.length; i++) {
 
             if (phone[i] > 0) {
@@ -20,6 +20,8 @@ public class Main {
                 temp++;
             }
         }
+        sum=sum/temp;
+        System.out.println("средне арифметическая "+sum);
 
 
     }
